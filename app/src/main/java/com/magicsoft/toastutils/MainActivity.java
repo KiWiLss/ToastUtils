49767c1a76c21cccbe068897a63940c62df4ca62
KiwiLss <m15205622327@163.com>
2018-01-10 18:05:37 +0800

    efficient

--- app/src/main/java/com/magicsoft/toastutils/MainActivity.java
+++ app/src/main/java/com/magicsoft/toastutils/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.magicsoft.mylibrary.Utils;
 import com.magicsoft.mylibrary.toast.ToastUtils;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Utils.init(this);
+
     }
 
     public void one(View view) {
@@ -20,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void one2(View view) {
-
-        ToastUtils.showShort("TWOTJWOTJWOTJO");
+        com.magicsoft.toastutils.utils.ToastUtils.showToast(getApplicationContext(),"TWOTJIT");
     }
 }
---
+++ mylibrary/src/main/java/com/magicsoft/mylibrary/toast/ToastEfficientUtils.java
@@ -0,0 +1,42 @@
+package com.magicsoft.mylibrary.toast;
+
+import android.widget.Toast;
+
+import com.magicsoft.mylibrary.Utils;
+
+public class ToastEfficientUtils {//原生的默认位置，高效
+      
+    /** 之前显示的内容 */  
+    private static String oldMsg ;  
+    /** Toast对象 */  
+    private static Toast toast = null ;
+    /** 第一次时间 */  
+    private static long oneTime = 0 ;  
+    /** 第二次时间 */  
+    private static long twoTime = 0 ;  
+      
+    /** 
+     * 显示Toast 
+     * @param
+     * @param message 
+     */  
+    public static void showToast( String message){
+        if(toast == null){  
+            toast = Toast.makeText(Utils.getContext(), message, Toast.LENGTH_SHORT);
+            toast.show() ;  
+            oneTime = System.currentTimeMillis() ;  
+        }else{  
+            twoTime = System.currentTimeMillis() ;  
+            if(message.equals(oldMsg)){  
+                if(twoTime - oneTime > Toast.LENGTH_SHORT){  
+                    toast.show() ;  
+                }  
+            }else{  
+                oldMsg = message ;  
+                toast.setText(message) ;  
+                toast.show() ;  
+            }  
+        }  
+        oneTime = twoTime ;  
+    }  
+}  
